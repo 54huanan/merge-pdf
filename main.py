1d17c3ffda17e3274e1640e6b85dbbaed47a7021
huanan <54huanan@163.com>
2021-10-04 07:59:58 +0800

    合并pdf文件

---
+++ main.py
@@ -0,0 +1,22 @@
+import getopt
+import os
+import sys
+from PyPDF2 import PdfFileMerger
+
+if __name__ == "__main__":
+    try:
+        options, args = getopt.getopt(sys.argv[1:], None)
+    except getopt.GetoptError as e:
+        print("Error => {}", e)
+        sys.exit()
+    target_dir = args[0]
+    pdf_lst = [os.path.join(root, file) for root, dirs, files in os.walk(target_dir) for file in files if
+               file.endswith(".pdf")]
+    print("文件列表：", pdf_lst)
+
+    file_merger = PdfFileMerger()
+    for pdf in pdf_lst:
+        file_merger.append(pdf)  # 合并pdf文件
+
+    file_merger.write(target_dir + "/merge.pdf")
+    print("合并成功")
